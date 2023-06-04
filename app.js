aad41feb85594b5beeff6cbbda948e514969c198
Ujjwal Garg <ujjwalgarg100204@gmail.com>
2023-06-04 13:36:49 +0530

    make site responsive

--- app.js
+++ app.js
@@ -1,51 +1,60 @@
 "use strict";
 
-const hour = document.querySelector("#hour");
-const minute = document.querySelector("#minute");
-const second = document.querySelector("#second");
-const amPM = document.querySelector("#am-pm");
-const hourDot = document.querySelector(".dot-1");
-const minDot = document.querySelector(".dot-2");
-const secDot = document.querySelector(".dot-3");
-const hourCircle = document.querySelector(".circle-1 > div[role=\"progressbar\"]");
-const minCircle = document.querySelector(".circle-2 > div[role=\"progressbar\"]");
-const secCircle = document.querySelector(".circle-3 > div[role=\"progressbar\"]");
+// Selectors
+const hourSpan = document.querySelector("#hour");
+const minSpan = document.querySelector("#minutes");
+const secSpan = document.querySelector("#seconds");
+const amPMSpan = document.querySelector("#ampm");
+const hourDot = document.querySelector("#dot-hour");
+const minDot = document.querySelector("#dot-minute");
+const secDot = document.querySelector("#dot-second");
+const hourContainer = hourDot.parentElement;
+const minContainer = minDot.parentElement;
+const secContainer = secDot.parentElement;
 
-
-function setText(element, text) {
-    element.textContent = text;
-}
-
-function padWithZeroes(number, length) {
-    let zeroes = "";
-
-    for (let i = 0; i < length - number.toString().length; i++)
-        zeroes += "0";
-
-    return zeroes + number.toString();   // concatenating zeroes to given number
-}
-
-function toRadian(deg) {
-    return deg * Math.PI / 180;
-}
-
-function moveDot(dot, circle, deg) {
-    dot.style.transform = `translate(${7.1 * Math.cos(toRadian(deg))}rem, ${7 * Math.sin(toRadian(deg))}rem)`;
-    circle.style.setProperty('--value', `${Math.abs((toRadian(deg + 90) / (2 * Math.PI)) * 100)}`);
-}
-
-
+// main clock logic
 setInterval(() => {
-        const now = new Date();
-        const h = now.getHours() % 12;
-        setText(hour, padWithZeroes(h, 2));
-        setText(minute, padWithZeroes(now.getMinutes(), 2));
-        setText(second, padWithZeroes(now.getSeconds(), 2));
-        setText(amPM, now.getHours() > 12 ? "PM" : "AM");
+	const now = new Date();
+	const time = {
+		hour: now.getHours(),
+		minute: now.getMinutes(),
+		sec: now.getSeconds(),
+		hour: 18,
+	};
 
-        moveDot(hourDot, hourCircle, 30 * h - 90);
-        moveDot(minDot, minCircle, 6 * now.getMinutes() - 90);
-        moveDot(secDot, secCircle, 6 * now.getSeconds() - 90);
-    },
-    1000
-);
+	// update time in document
+	hourSpan.textContent = padWithZeroes(time.hour, 2);
+	minSpan.textContent = padWithZeroes(time.minute, 2);
+	secSpan.textContent = padWithZeroes(time.sec, 2);
+	amPMSpan.textContent = time.hour < 12 ? "AM" : "PM";
+
+	// update bar fill
+	hourContainer.style.setProperty("--p", `${(time.hour / 24) * 100}`);
+	minContainer.style.setProperty("--p", `${(time.minute / 60) * 100}`);
+	secContainer.style.setProperty("--p", `${(time.sec / 60) * 100}`);
+
+	// update dot location
+	hourDot.style.transform = `rotate(${
+		(time.hour * 360) / 24 + time.hour * 360
+	}deg)`;
+	minDot.style.transform = `rotate(${
+		(time.minute * 360) / 60 + time.hour * 360
+	}deg)`;
+	secDot.style.transform = `rotate(${
+		(time.sec * 360) / 60 + time.minute * 360
+	}deg)`;
+}, 1000);
+
+// utility functions
+/**
+ *
+ * @param {number} number the number which you want to pad with zeroes to left
+ * @param {number} length total length of number including zeroes
+ * @returns {string}
+ */
+function padWithZeroes(number, length) {
+	const zeroes = Array(length - number.toString().length)
+		.fill("0")
+		.join("");
+	return zeroes + number.toString(); // concatenating zeroes to given number
+}
